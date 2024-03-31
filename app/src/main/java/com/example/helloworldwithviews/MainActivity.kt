5b3112ebef8956abbe123bf25e1f5e6a2bdc8b15
IvanFlores <ifloresmanc96@gmail.com>
2024-03-31 13:28:49 -0300

    clean project

--- app/src/main/java/com/example/helloworldwithviews/MainActivity.kt
+++ app/src/main/java/com/example/helloworldwithviews/MainActivity.kt
@@ -5,19 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.helloworldwithviews.adapter.SuperHeroAdapter
-import com.example.helloworldwithviews.superHeroModel.SuperHeroModel
-import com.example.helloworldwithviews.superHeroProvider.SuperHeroProvider
 
 class MainActivity : AppCompatActivity() {
-
-    val superHero = SuperHeroModel(
-        "Ivan",
-        "Marvel",
-        "httop..."
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,12 +17,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        initRecycleView()
     }
 
-    fun initRecycleView(){
-        var recycleView = findViewById<RecyclerView>(R.id.superHeroRecyclerView)
-        recycleView.layoutManager = LinearLayoutManager(this)
-        recycleView.adapter = SuperHeroAdapter(SuperHeroProvider.heroesList)
-    }
 }
--- app/src/main/java/com/example/helloworldwithviews/superHeroModel/SuperHeroModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.helloworldwithviews.superHeroModel
-
-data class SuperHeroModel(
-    val name: String,
-    val author: String,
-    val imageUrl: String
-)
